290694894831e3d22b563fbffd3783209b9a6c12
Allison Blumenthal <blumen.allison@gmail.com>
2023-02-27 20:13:51 -0500

    api calls for members

---
+++ api/memberData.js
@@ -0,0 +1,84 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// GET ALL MEMBERS
+const getAllMembers = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+// GET SINGLE MEMBER
+const getSingleMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+// CREATE MEMBER
+const createMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+// UPDATE MEMBER
+const updateMember = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
+// DELETE MEMBER
+const deleteMember = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/members/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+export {
+  getAllMembers,
+  getSingleMember,
+  createMember,
+  updateMember,
+  deleteMember,
+};
