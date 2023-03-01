73bd567eb3bb54ec4d6a7b4e14254c01e74d0a93
Allison Blumenthal <blumen.allison@gmail.com>
2023-02-28 21:42:56 -0500

    added member pages and member form

---
+++ pages/member/new.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function AddMember() {
+  return (
+    <>
+      <Head>
+        <title>New Household Member</title>
+      </Head>
+      <div>
+        {/* <MemberForm /> */}
+      </div>
+    </>
+  );
+}
