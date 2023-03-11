d6de2dcc7bea8f9b7db3596adb9ba4c767a5c046
Allison Blumenthal <blumen.allison@gmail.com>
2023-03-10 22:30:26 -0500

    index page routes to member's household details page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Head from 'next/head';
+import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+import { useAuth } from '../utils/context/authContext';
+import { getMemberByUID } from '../api/memberData';
 
-export default function index() {
+export default function Index() {
+  const [member, setMember] = useState([]);
+  const router = useRouter();
+  const { uid } = useAuth();
+
+  useEffect(() => {
+    getMemberByUID(uid).then(setMember);
+  }, [uid]);
+
   return (
-    <div>
-      <h1>Welcome to Keepet!</h1>
-      <h3>A place to keep your pets and people organized.</h3>
-    </div>
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <div>
+        <Button type="btn" className="mx-2 primary" onClick={() => router.push(`/household/${member[0].householdId}`)}>Go to My Household</Button>
+      </div>
+    </>
   );
 }
