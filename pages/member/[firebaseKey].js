1dda0a4bceb050906b2f7a4f1697298700d67b56
Allison Blumenthal <blumen.allison@gmail.com>
2023-03-12 16:38:31 -0400

    added edit button to member details page - only visible to member or admin

--- pages/member/[firebaseKey].js
+++ pages/member/[firebaseKey].js
@@ -2,15 +2,27 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
-import { getSingleMember } from '../../api/memberData';
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { getSingleMember, getMemberByUID } from '../../api/memberData';
+import { useAuth } from '../../utils/context/authContext';
 
 export default function ViewMember() {
+  const [member, setMember] = useState({});
   const [memberDetails, setMemberDetails] = useState([]);
   const router = useRouter();
+  const { user } = useAuth();
 
   const { firebaseKey } = router.query;
 
+  const getMemberInfo = () => {
+    getMemberByUID(user.uid).then((memberObj) => {
+      setMember(memberObj[0]);
+    });
+  };
+
   useEffect(() => {
+    getMemberInfo();
     getSingleMember(firebaseKey).then(setMemberDetails);
   }, [firebaseKey]);
 
@@ -19,6 +31,13 @@ export default function ViewMember() {
       <Head>
         <title>{memberDetails?.title}</title>
       </Head>
+      {(memberDetails.uid === member.uid) || (member.isAdmin === true) ? (
+        <>
+          <Link href={`/member/edit/${firebaseKey}`} passHref>
+            <Button variant="info" className="edit-btn">EDIT</Button>
+          </Link>
+        </>
+      ) : ''}
       <div className="mt-5 d-flex flex-wrap">
         <div className="d-flex flex-column">
           <img src={`/assets/images/memberAvatars/${memberDetails.memberAvatar}`} alt={memberDetails.memberName} style={{ width: '300px' }} />
